354a9765184e7ff1e339485682274e8f5ad0076a
Eduard <eazymov@devvela.com>
2017-11-16 17:19:29 +0500

    WIP

--- js/main.js
+++ js/main.js
@@ -97,7 +97,7 @@ var assetList = [];
 //	TODO
 //	use underscore and ".after" to load these in order
 //	don't look at me I'm ugly
-function start( e ){	
+function start (e) {	
 	//	detect for webgl and reject everything else
 	if ( ! Detector.webgl ) {
 		Detector.addGetWebGLMessage();
@@ -111,11 +111,11 @@ function start( e ){
 			mapOutlineImage.src = 'images/map_outline.png';
 			mapOutlineImage.onload = function(){
 				loadCountryCodes(
-					function(){
+					function () {
 						loadWorldPins(
-							function(){										
+							function () {										
 								loadContentData(								
-									function(){																	
+									function () {																	
 										initScene();
 										animate();		
 									}
@@ -181,13 +181,13 @@ function initScene() {
 	light1.position.y = 520;
 	light1.position.z = 626;
 	light1.castShadow = true;
-	scene.add( light1 );
+	scene.add(light1);
 
 	light2 = new THREE.PointLight( 0x222222, 14.8 );
 	light2.position.x = -640;
 	light2.position.y = -500;
 	light2.position.z = -1000;
-	scene.add( light2 );				
+	scene.add(light2);				
 
 	rotating = new THREE.Object3D();
 	scene.add(rotating);
@@ -221,70 +221,47 @@ function initScene() {
 	mapUniforms = uniforms;
 
 	var shaderMaterial = new THREE.ShaderMaterial( {
-
-		uniforms: 		uniforms,
-		// attributes:     attributes,
-		vertexShader:   document.getElementById( 'globeVertexShader' ).textContent,
+		uniforms: uniforms,
+		vertexShader: document.getElementById( 'globeVertexShader' ).textContent,
 		fragmentShader: document.getElementById( 'globeFragmentShader' ).textContent,
-		// sizeAttenuation: true,
 	});
 
-
-    //	-----------------------------------------------------------------------------
-    //	Create the backing (sphere)
-    // var mapGraphic = new THREE.Texture(worldCanvas);//THREE.ImageUtils.loadTexture("images/map.png");
-    // backTexture =  mapGraphic;
-    // mapGraphic.needsUpdate = true;
 	backMat = new THREE.MeshBasicMaterial(
 		{
-			// color: 		0xffffff, 
-			// shininess: 	10, 
-// 			specular: 	0x333333,
-			// map: 		mapGraphic,
-			// lightMap: 	mapGraphic
+
 		}
 	);				
-	// backMat.ambient = new THREE.Color(255,255,255);							
+							
 	sphere = new THREE.Mesh( new THREE.SphereGeometry( 100, 40, 40 ), shaderMaterial );				
-	// sphere.receiveShadow = true;
-	// sphere.castShadow = true;
+
 	sphere.doubleSided = false;
 	sphere.rotation.x = Math.PI;				
 	sphere.rotation.y = -Math.PI/2;
 	sphere.rotation.z = Math.PI;
 	sphere.id = "base";	
-	rotating.add( sphere );	
+	rotating.add(sphere);	
 
-
-	for( var i in timeBins ){					
+	for (var i in timeBins){					
 		var bin = timeBins[i].data;
-		for( var s in bin ){
+		for(var s in bin) {
 			var set = bin[s];
-			// if( set.v < 1000000 )
-			// 	continue;
-
 			var exporterName = set.e.toUpperCase();
 			var importerName = set.i.toUpperCase();
 
-			//	let's track a list of actual countries listed in this data set
-			//	this is actually really slow... consider re-doing this with a map
-			if( $.inArray(exporterName, selectableCountries) < 0 )
-				selectableCountries.push( exporterName );
+			if($.inArray(exporterName, selectableCountries) < 0)
+				selectableCountries.push(exporterName);
 
-			if( $.inArray(importerName, selectableCountries) < 0 )
-				selectableCountries.push( importerName );
+			if($.inArray(importerName, selectableCountries) < 0)
+				selectableCountries.push(importerName);
 		}
 	}
-
-	console.log( selectableCountries );
 	
-	// load geo data (country lat lons in this case)
 	console.time('loadGeoData');
-	loadGeoData( latlonData );				
+	loadGeoData(latlonData);				
 	console.timeEnd('loadGeoData');				
 
 	console.time('buildDataVizGeometries');
-	var vizilines = buildDataVizGeometries(timeBins);
+	buildDataVizGeometries(timeBins);
 	console.timeEnd('buildDataVizGeometries');
 
 	visualizationMesh = new THREE.Object3D();
@@ -300,7 +277,7 @@ function initScene() {
 
     //	-----------------------------------------------------------------------------
     //	Setup our renderer
-	renderer = new THREE.WebGLRenderer({antialias:false});
+	renderer = new THREE.WebGLRenderer({ antialias:false });
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	renderer.autoClear = false;
 	
@@ -331,9 +308,9 @@ function initScene() {
 
 	document.addEventListener( 'keydown', onKeyDown, false);												    			    	
 
-    //	-----------------------------------------------------------------------------
-    //	Setup our camera
-    camera = new THREE.PerspectiveCamera( 12, window.innerWidth / window.innerHeight, 1, 20000 ); 		        
+	//	-----------------------------------------------------------------------------
+	//	Setup our camera
+	camera = new THREE.PerspectiveCamera( 12, window.innerWidth / window.innerHeight, 1, 20000 ); 		        
 	camera.position.z = 1400;
 	camera.position.y = 0;
 	camera.lookAt(scene.width/2, scene.height/2);	
@@ -343,32 +320,14 @@ function initScene() {
 }
 	
 
-function animate() {	
+function animate () {
 
-	//	Disallow roll for now, this is interfering with keyboard input during search
-/*	    	
-	if(keyboard.pressed('o') && keyboard.pressed('shift') == false)
-		camera.rotation.z -= 0.08;		    	
-	if(keyboard.pressed('p') && keyboard.pressed('shift') == false)
-		camera.rotation.z += 0.08;		   
-*/
-
-	if( rotateTargetX !== undefined && rotateTargetY !== undefined ){
+	if (rotateTargetX !== undefined && rotateTargetY !== undefined) {
 
 		rotateVX += (rotateTargetX - rotateX) * 0.012;
 		rotateVY += (rotateTargetY - rotateY) * 0.012;
 
-		// var move = new THREE.Vector3( rotateVX, rotateVY, 0 );
-		// var distance = move.length();
-		// if( distance > .01 )
-		// 	distance = .01;
-		// move.normalize();
-		// move.multiplyScalar( distance );
-
-		// rotateVX = move.x;
-		// rotateVy = move.y;		
-
-		if( Math.abs(rotateTargetX - rotateX) < 0.1 && Math.abs(rotateTargetY - rotateY) < 0.1 ){
+		if (Math.abs(rotateTargetX - rotateX) < 0.1 && Math.abs(rotateTargetY - rotateY) < 0.1) {
 			rotateTargetX = undefined;
 			rotateTargetY = undefined;
 		}
@@ -376,8 +335,6 @@ function animate() {
 	
 	rotateX += rotateVX;
 	rotateY += rotateVY;
-
-	//rotateY = wrap( rotateY, -Math.PI, Math.PI );
 
 	rotateVX *= 0.98;
 	rotateVY *= 0.98;
@@ -389,13 +346,12 @@ function animate() {
 
 	rotateY += controllers.spin * 0.01;
 
-	//	constrain the pivot up/down to the poles
-	//	force a bit of bounce back action when hitting the poles
-	if(rotateX < -rotateXMax){
+	if (rotateX < -rotateXMax){
 		rotateX = -rotateXMax;
 		rotateVX *= -0.95;
 	}
-	if(rotateX > rotateXMax){
+
+	if (rotateX > rotateXMax){
 		rotateX = rotateXMax;
 		rotateVX *= -0.95;
 	}		    			    		   
@@ -405,29 +361,26 @@ function animate() {
 	rotating.rotation.x = rotateX;
 	rotating.rotation.y = rotateY;	
 
-    render();	
-    		        		       
-    requestAnimationFrame( animate );	
+	render();	       		       
+	requestAnimationFrame(animate);	
 
-
-	THREE.SceneUtils.traverseHierarchy( rotating, 
-		function(mesh) {
+	THREE.SceneUtils.traverseHierarchy(rotating, 
+		function (mesh) {
 			if (mesh.update !== undefined) {
 				mesh.update();
 			} 
 		}
 	);	
 
-	for( var i in markers ){
+	for (var i in markers) {
 		var marker = markers[i];
 		marker.update();
-	}		    	
-
+	}
 }
 
 function render() {	
-	renderer.clear();		    					
-    renderer.render( scene, camera );				
+	renderer.clear();    					
+	renderer.render(scene, camera);			
 }		   
 
 function findCode(countryName){
